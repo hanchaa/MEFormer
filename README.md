f4898ddf2fb825a89c932d2999cb4d7c468dacb9
Juhan Cha <hanchaa@gmail.com>
2025-01-22 10:36:51 +0900

    chore(main): add create_data shell script

--- README.md
+++ README.md
@@ -47,7 +47,10 @@ MEFormer
 ```
 
 #### 4. Prepare data
-Follow the instructions from [mmdet3d](https://github.com/open-mmlab/mmdetection3d/blob/1.0/docs/en/data_preparation.md) for data preparation.
+Run [create_data.sh](https://github.com/hanchaa/MEFormer/blob/main/tools/create_data.sh) script.
+```shell
+bash tools/create_data.sh
+```
 
 ## Train & Inference
 #### Train
--- tools/create_data.sh
+++ tools/create_data.sh
@@ -3,22 +3,4 @@
 set -x
 export PYTHONPATH=`pwd`:$PYTHONPATH
 
-PARTITION=$1
-JOB_NAME=$2
-DATASET=$3
-GPUS=${GPUS:-1}
-GPUS_PER_NODE=${GPUS_PER_NODE:-1}
-SRUN_ARGS=${SRUN_ARGS:-""}
-JOB_NAME=create_data
-
-srun -p ${PARTITION} \
-    --job-name=${JOB_NAME} \
-    --gres=gpu:${GPUS_PER_NODE} \
-    --ntasks=${GPUS} \
-    --ntasks-per-node=${GPUS_PER_NODE} \
-    --kill-on-bad-exit=1 \
-    ${SRUN_ARGS} \
-    python -u tools/create_data.py ${DATASET} \
-            --root-path ./data/${DATASET} \
-            --out-dir ./data/${DATASET} \
-            --extra-tag ${DATASET}
+python tools/create_data.py nuscenes --root-path ./data/nuscenes/ --out-dir ./data/nuscenes --extra-tag nuscenes
